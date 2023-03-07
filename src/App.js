ca0d517285c401be941fe3b72f076f48ed77a936
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-07 22:21:01 +0200

    Update App.js and calculator and packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import {
-  BrowserRouter, Routes, Route, Outlet, Link,
+  BrowserRouter, Routes, Route, NavLink,
 } from 'react-router-dom';
 import './Calculator.css';
-import AppCalculator from './Components/Calculator';
+// pages
+// import AppCalculator from './Components/Calculator';
 import Home from './pages/Home';
-
-i;
+import Quote from './pages/Quote';
+import CalculatorPage from './pages/CalculatorPage';
 
 function App() {
   return (
-    <AppCalculator />
+    <BrowserRouter>
+
+      <header>
+        <nav>
+          <h1>Math Magicians</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/CalculatorPage">Calculator</NavLink>
+          <NavLink to="/Quote">Quote</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/CalculatorPage" element={<CalculatorPage />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,11 +1,11 @@
 import '../Calculator.css';
 import Calculator from '../Components/Calculator';
 
-const CalculatorPage = () => {
+const CalculatorPage = () => (
   <div>
     <h4 className="page-calculator">Let do some math</h4>
     <Calculator />
-  </div>;
-};
+  </div>
+);
 
 export default CalculatorPage;
