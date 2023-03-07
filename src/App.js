d6b672624a64905e47340f9b0abf98827f47fc8a
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-07 23:02:59 +0200

    Add style and Fix linters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
 } from 'react-router-dom';
 import './Calculator.css';
 // pages
-// import AppCalculator from './Components/Calculator';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 import CalculatorPage from './pages/CalculatorPage';
@@ -13,10 +12,12 @@ function App() {
     <BrowserRouter>
 
       <header>
+        <h1>Math Magicians</h1>
         <nav>
-          <h1>Math Magicians</h1>
           <NavLink to="/">Home</NavLink>
+          <span>|</span>
           <NavLink to="/CalculatorPage">Calculator</NavLink>
+          <span>|</span>
           <NavLink to="/Quote">Quote</NavLink>
         </nav>
       </header>
