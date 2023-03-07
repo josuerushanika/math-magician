db0ece1f35e7f2af7a66dbe54af8a1628a115e00
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-07 20:07:54 +0200

    Adding Quote

---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import '../Calculator.css';
+
+export default function Quote() {
+  return (
+    <div>
+      <p className="quote">
+        Mathematics is not about numbers, equations or algorithms: it is about
+        understanding. - William Paul Thurston
+      </p>
+    </div>
+  );
+}
