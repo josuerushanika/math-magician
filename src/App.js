ab7d68154194a4ec25943ca45bdb8c2b692897db
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-07 20:36:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import {
+  BrowserRouter, Routes, Route, Outlet, Link,
+} from 'react-router-dom';
 import './Calculator.css';
 import AppCalculator from './Components/Calculator';
+import Home from './pages/Home';
+
+i;
 
 function App() {
   return (
